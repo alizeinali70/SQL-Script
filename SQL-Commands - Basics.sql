14bc8743474e822fcfbb85c0611ebdc7b92b969b
Ali <ali.zeinali70@gmail.com>
2025-11-18 20:56:26 +0100

    Add files via upload

---
+++ SQL-Commands - Basics.sql
@@ -0,0 +1,54 @@
+CREATE TABLE Students
+(Id int IDENTITY(1,1) primary KEY,
+Fullname NVARCHAR(50),
+Major NVARCHAR(50),
+Average Float,
+City NVARCHAR(50)
+);
+
+INSERT INTO Students (Fullname, Major, Average, City)
+Values
+(N'مریم احمدی', N'مهندسی کامپیوتر', 18.5, N'تهران'),
+(N'سینا رضایی', N'ریاضی کاربردی', 16.2, N'اصفهان'),
+(N'ندا کریمی', N'مدیریت صنعتی', 14.8, N'شیراز'),
+(N'محمد مرادی', N'مهندسی نرم‌افزار', 19.1, N'تبریز'),
+(N'رضا یوسفی', N'حسابداری', 12.5, N'مشهد');
+
+
+Select * from students;
+
+
+select * from Students
+ where Average > 17;
+
+
+select * from Students
+where Fullname LIKE N'%ن%';
+
+Update Students
+SET AVERAGE = 16
+where ID= 3;
+
+DELETE FROM Students
+where ID= 5;
+
+
+
+
+INSERT INTO Students (Fullname, Major, Average, City)
+Values
+(N'رضا احمدی', N'مهندسی برق', 17.5, N'شیراز');
+
+
+
+Select TOP 3 * from students 
+order by Average DESC;
+
+SELECT count(*) from Students;
+
+SELECT count(*) as Studentscount from Students;
+
+
+Select AVG(average) as AVGStudents from Students;
+
+select * from Students;
